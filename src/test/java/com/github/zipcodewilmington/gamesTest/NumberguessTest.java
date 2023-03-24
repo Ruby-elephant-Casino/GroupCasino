619292463c68417d206aac755ebd50dcbb4ca670
FatimaUppal <125939989+FatimaUppal@users.noreply.github.com>
2023-03-24 14:02:04 -0400

    number guess test

--- src/test/java/com/github/zipcodewilmington/gamesTest/NumberguessTest.java
+++ src/test/java/com/github/zipcodewilmington/gamesTest/NumberguessTest.java
@@ -1,4 +1,37 @@
 package com.github.zipcodewilmington.gamesTest;
 
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.games.numberguess.NumberGuessGame;
+import com.github.zipcodewilmington.casino.games.numberguess.NumberGuessPlayer;
+import com.github.zipcodewilmington.casino.games.roulette.RouletteGame;
+import com.github.zipcodewilmington.casino.games.roulette.RoulettePlayer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 public class NumberguessTest {
+//    @Test
+//    public void add() {
+//        // Arrange
+//
+//        NumberGuessPlayer player = new NumberGuessPlayer(new CasinoAccount());
+//
+//        NumberGuessGame game = new NumberGuessGame();
+//        game.add(player);
+//        assertNotNull(game.getCurrentPlayer());
+//
+//    }
+//
+//
+//    @Test
+//    public void remove() {
+//        NumberGuessPlayer player = new NumberGuessPlayer(new CasinoAccount());
+//
+//        NumberGuessGame game = new NumberGuessGame();
+//        game.add(player);
+//        game.remove(player);
+//        assertNull(game.getCurrentPlayer());
+//
+//    }
 }
